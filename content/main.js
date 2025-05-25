b2bb1e09256274d9075c160d05d8759a6f6b266d
XSkeetty <86146873+XSkeetty@users.noreply.github.com>
2025-05-26 02:42:33 +1100

    Documentation edits made through Mintlify web editor

--- content/main.js
+++ content/main.js
@@ -1,7 +1,6 @@
-console.log("Пользовательский скрипт загружен!");
-window.addEventListener('load', function() {
+setTimeout(function () {
   const loader = document.getElementById('loader');
   if (loader) {
     loader.style.display = 'none';
   }
-});
+}, 5000); // 5000 миллисекунд = 5 секунд
