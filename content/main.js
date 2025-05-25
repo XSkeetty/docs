5ae6bfac12b9e10e610b291f9a538a3cea647d42
XSkeetty <86146873+XSkeetty@users.noreply.github.com>
2025-05-26 02:33:33 +1100

    Documentation edits made through Mintlify web editor

--- content/main.js
+++ content/main.js
@@ -1,1 +1,7 @@
 console.log("Пользовательский скрипт загружен!");
+window.addEventListener('load', function() {
+  const loader = document.getElementById('loader');
+  if (loader) {
+    loader.style.display = 'none';
+  }
+});
