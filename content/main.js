cc9332c003edde0c538b73be5093c94b3fdbba79
XSkeetty <86146873+XSkeetty@users.noreply.github.com>
2025-05-26 13:02:02 +1100

    Documentation edits made through Mintlify web editor

--- content/main.js
+++ content/main.js
@@ -1,6 +1,22 @@
-console.log("Скрипт загружен!")
-const loader = document.getElementById('loader');
-if (loader) {
-  console.log("Loader найден!")
-  loader.style.display = 'none';
-}
+// Переопределение метода document.createElement
+(function() {
+  const originalCreateElement = document.createElement;
+  document.createElement = function(tagName) {
+    const element = originalCreateElement.call(document, tagName);
+    if (tagName.toLowerCase() === 'script') {
+      Object.defineProperty(element, 'src', {
+        set: function(value) {
+          if (value.includes('cdn.lr-in-prod.com/logger-1.min.js')) {
+            console.warn('Блокировка загрузки скрипта:', value);
+          } else {
+            element.setAttribute('src', value);
+          }
+        },
+        get: function() {
+          return element.getAttribute('src');
+        }
+      });
+    }
+    return element;
+  };
+})();
