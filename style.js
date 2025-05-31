955fbb92fc9d535d2494b11d230e5f7555859db1
XSkeetty <86146873+XSkeetty@users.noreply.github.com>
2025-05-31 12:59:55 +1100

    Documentation edits made through Mintlify web editor

--- style.js
+++ style.js
@@ -7,16 +7,13 @@
     allSpans.forEach((span) => {
       // Если текст точно “on this page” (игнорируем регистр/пробелы)
       if (span.innerText && span.innerText.trim().toLowerCase() === 'on this page') {
-        console.log('   Найден <span>:', span);
         span.innerText = 'На этой странице';
-        console.log('   Текст у этого <span> заменён на "На этой странице"');
       }
     });
   }
 
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
-      console.log('DOMContentLoaded сработал, вызываем replaceAllOnThisPage()');
       replaceAllOnThisPage();
     });
   } else {
